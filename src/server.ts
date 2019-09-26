2fcc91d7f1f63876a5f98e42ce7dda6faeaee744
vasanthakumarn <41970169+vasanthakumarn@users.noreply.github.com>
2019-09-26 19:25:23 +0530

    Test
    Test

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express = require('express');
 const server = express();
 
 server.get('/', (request: express.Request, response: express.Response) => {
-    response.status(200).send('hello world - sample!!!');
+    response.status(200).send('hello world - sample-heroku!!!');
 });
 console.log('Server is listening...');
 module.exports = server.listen(process.env.PORT || 3000);
