a96704248ce811f6f2746db84e8069a668d8c710
VasanthakumarNagamuthu <vasanth.n@ecgroup-intl.com>
2019-09-26 21:06:32 +0530

    'trial1'

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,42 @@
 import express = require('express');
-
+const libreOfficeConvert = require('libreoffice-convert');
+const fs = require('fs');
+const path = require('path');
 const server = express();
+const outputFileName = 'Sample2';
+const extend='.pdf';
 
 server.get('/', (request: express.Request, response: express.Response) => {
-    response.status(200).send('hello world - sample-heroku!!!');
+	const enterPath = path.join(__dirname, ${outputFileName}.docx);
+       const outputPath = path.join(__dirname, ${outputFileName}.pdf);
+       const readEnterPath = fs.readFileSync(enterPath);
+	
+	libreOfficeConvert.convert(readEnterPath, extend, undefined, (err: any, result: any) => {
+           console.log('libreOfficeConvert....');
+           if (err) {
+               console.log(Error converting file: ${err});
+               return  {filePath: '', pdfFileName: ''};
+           }
+           console.log('result.....', result);
+           if (result) {
+               fs.writeFileSync(path.resolve(__dirname , ${outputFileName}.pdf), result);
+               console.log('__dirname   ', __dirname);
+               console.log('outputFileName   ', outputFileName);
+               return response.status(200).sendFile(outputPath, '',  (error: any) => {
+                   if (error) {
+                       console.log(error);
+                   } else {
+                       fs.unlinkSync(${__dirname}\\${outputFileName}.docx);
+                       fs.unlinkSync(${__dirname}\\${outputFileName}.pdf);
+                   }
+               });
+           } else {
+               return  {filePath: '', pdfFileName: ''};
+			   response.status(200).send('hello world - sample-heroku!!!');
+           }
+       });
+	
+    //response.status(200).send('hello world - sample-heroku!!!');
 });
 console.log('Server is listening...');
 module.exports = server.listen(process.env.PORT || 3000);
